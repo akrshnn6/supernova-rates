1b02268db872ed703cea0ba5e16eb302ad55c897
Ashvini Krishnan <akrshnn6@illinois.edu>
2019-02-13 20:39:06 -0600

    Add files via upload
    exported csv file of all explosion rates for a range of distances

--- data export - all rates.py
+++ data export - all rates.py
@@ -10,30 +10,46 @@ import numpy as np
 from astropy.table import Table, Column
 from astropy import units as u
 from integral_3D import rates
-
-#distribution of radii
-r = np.logspace(1,10, num=100, base = 10)
-#rate of supernovae
+#%%
+#distribution of radii in pc from 1 (essentially the center) to 2.62^10
+r = np.logspace(1, 2.62, num=100, base = 10)
 R_sn = rates.R_super
 R_lgrb = rates.R_lgrb
 R_sgrb = rates.R_sgrb
 R = [R_sn, R_lgrb, R_sgrb]
 
-
+#rates for each explosion
 rate_sn = []
 rate_lgrb = []
 rate_sgrb = []
 rate = [rate_sn, rate_lgrb, rate_sgrb]
 
+#integration error for each explosion
 error_sn = []
 error_sgrb = []
 error_lgrb = []
 error = [error_sn, error_lgrb, error_sgrb]
+#%%
 
 for r_x in r:
-    for R_x in R:
-        for i in range(0,2):
-            rate[i].append(rates.integral(r_x, R_x)[0])
-            error[i].append(rates.integral(r_x, R_x)[1])
+    for i in range(0,3):
+        rate[i].append(rates.integral(r_x, R[i])[0])
+        error[i].append(rates.integral(r_x, R[i])[1])
         
-data = Table([rate_sn, error_sn, rate_lgrb, error_lgrb, rate_sgrb, error_lgrb], names = ['0'])
+#%%
+        
+
+#%%
+data_points = []
+for i in range(0,3):
+    data_points.append(rate[i])
+    data_points.append(error[i])
+    
+name = ['Rate of Supernovae','Error of Supernovae','Rate of LGRB','Error of LGRB','Rate of SGRB','Error of SGRB']
+data = Table(data_points, names = name)
+data.add_column(Column(r, name = 'Distance'), index = 0)
+
+#%%
+ascii.write(data, 'rates.csv', format = 'csv', fast_writer = False, overwrite = True)
+
+
