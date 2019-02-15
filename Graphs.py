708494205f9bda78be8c2f72f1129a10235a435a
Ashvini Krishnan <akrshnn6@illinois.edu>
2019-02-15 16:16:12 -0600

    Add files via upload

--- Graphs.py
+++ Graphs.py
@@ -38,27 +38,29 @@ plt.xlabel('Distances (pc)')
 plt.tight_layout()
 plt.savefig('rates vs distances.png')
 #%%
-rates_exp = ascii.read('fluences.csv')
+fluences = ascii.read('fluences.csv')
 r = rates_exp['Distance']
 
 plt.figure(figsize = (10,15))
 
 plt.subplot(311)
-plt.loglog(rates_exp['Fluence of Supernovae'], rates_exp['Distance'], color = 'b')
+plt.loglog(fluences['Fluence of Supernovae'], rates_exp['Rate of Supernovae'], color = 'b')
 plt.title('Fluence of Supernovae vs Distance')
 plt.xlabel('Fluence (erg/pc^2)')
 plt.ylabel('Distances (pc)')
 
 plt.subplot(312)
-plt.loglog(rates_exp['Fluence of LGRB'], rates_exp['Distance'], color = 'g')
+plt.loglog(fluences['Fluence of LGRB'], rates_exp['Rate of LGRB'], color = 'g')
 plt.title('Fluence of LGRB vs Distance')
 plt.xlabel('Fluence (erg/pc^2)')
 plt.ylabel('Distances (pc)')
 
 plt.subplot(313)
-plt.loglog(rates_exp['Fluence of SGRB'], rates_exp['Distance'], color = 'r')
+plt.loglog(fluences['Fluence of SGRB'], rates_exp['Rate of SGRB'], color = 'r')
 plt.title('Fluence of SGRB vs Distance')
 plt.xlabel('Fluence (erg/pc^2)')
 plt.ylabel('Distances (pc)')
 
 plt.tight_layout()
+plt.savefig('fluences vs rates.png')
+
